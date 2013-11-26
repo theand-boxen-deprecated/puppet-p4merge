593d5f76fbe50c86714024d9b8a9cf9dd7fb285b
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2013-11-26 18:09:20 +0100

    Added liunk to executable

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,4 +8,10 @@ class p4merge {
     provider => 'appdmg',
     source   => 'http://www.perforce.com/downloads/perforce/r13.3/bin.macosx106x86_64/P4V.dmg'
   }
+
+  file { '/opt/boxen/bin/p4merge': 
+    ensure => link,
+    target  => '/Applications/p4merge.app/Contents/Resources/launchp4merge',
+    require => Package['p4merge'];
+  }
 }
